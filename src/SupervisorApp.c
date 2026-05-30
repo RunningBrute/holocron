8f06a72140133f0c5f3d15d0d8545b39fa29c70d
RunningBrute <piotrekbas8@gmail.com>
2026-05-31 00:31:57 +0200

    add parameter to creating whole batch of nodes at once

--- src/SupervisorApp.c
+++ src/SupervisorApp.c
@@ -17,30 +17,42 @@ int main(int argc, char* argv[])
 
     if (argc < 2)
     {
-        printf("[Supervisor] Too few arguments to start node! Nodes numbers needed! \n");
-        return -1;
-    }
-
-    int nodesNum = atoi(argv[1]);
-    int pid = fork();
-
-    int nodesHandles[MAX_NODES] = {0};
-
-    if (isNewProcess(pid))
-    {
-        printf("[Supervisor] Place for Node app \n");
-        char* const nodeArgv[] = {"node", "2", NULL};
-        nodesHandles[0] = execv("node", nodeArgv);
-
-        perror("[ERROR] execv");
+        perror("[Supervisor][ERROR] Too few arguments to start node! Nodes numbers needed! \n");
         exit(EXIT_FAILURE);
     }
-    else
+
+    int nodesCount = atoi(argv[1]);
+    if (nodesCount > MAX_NODES)
     {
-        printf("[Supervisor] Old process \n");
+        nodesCount = MAX_NODES;
+        printf("[Supervisor][WARNING] Too many nodes. Only %d nodes will be created\n", MAX_NODES);
+    }
+    printf("[Supervisor] %d nodes will be created\n", nodesCount);
+
+    int nodesHandles[MAX_NODES + 1] = {0};
+
+    for (int id = 1; id <= nodesCount; ++id)
+    {
+        int pid = fork();
+        if (isNewProcess(pid))
+        {
+            printf("[Supervisor] Trying to create new node with ID %d \n", id);
+
+            char textId[20];
+            snprintf(textId, sizeof(textId), "%d", id);
+
+            char* const nodeArgv[] = {"node", textId, NULL};
+            nodesHandles[id] = execv("node", nodeArgv);
+
+            perror("[Supervisor][ERROR] execv");
+            exit(EXIT_FAILURE);
+        }
     }
 
-    waitpid(nodesHandles[0], NULL, 0);
+    for (int id = 1; id <= nodesCount; ++id)
+    {
+        waitpid(nodesHandles[id], NULL, 0);
+    }
     printf("[Supervisor] Supervisor ended. \n");
 
     return 0;
