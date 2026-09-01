2b075f038d57494dddbacf01423010f11c092ef7
RunningBrute <piotrekbas8@gmail.com>
2026-09-02 00:10:42 +0200

    add test app written in qt/qml

---
+++ qt-test/main.cpp
@@ -0,0 +1,13 @@
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include <QUrl>
+
+int main(int argc, char *argv[])
+{
+    QGuiApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+    engine.load(QUrl(QStringLiteral("qrc:/QtTest/Main.qml")));
+
+    return app.exec();
+}
