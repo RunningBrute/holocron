cd95d00562ac40fba329fb45df6933eb1f39d497
RunningBrute <piotrekbas8@gmail.com>
2026-09-08 21:44:15 +0200

    parse socket adress from argument list

--- apps/node/NodeApp.c
+++ apps/node/NodeApp.c
@@ -1,27 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 
 #include "holocron/node/Node.h"
 
+typedef struct SocketPath
+{
+    const char* path;
+    size_t length;
+} SocketPath;
+
 int main(int argc, char* argv[])
 {
-    if (argc < 2)
+    if (argc < 3)
     {
-        printf("[Node] Too few arguments to start node! Node ID needed! \n");
+        printf("[Node] Too few arguments to start node! Node ID and socket path needed! \n");
         return -1;
     }
+
+    SocketPath socketPath;
+    socketPath.path = argv[2];
+    socketPath.length = strlen(argv[2]);
 
     int id = atoi(argv[1]);
     int pid = getpid();
 
     Node* node = createNode(id);
     printf("[Node] Node with ID %d and PID %d created \n", id, pid);
+    printf("[Node] socket adress: %s \n", socketPath.path);
 
     while (1)
     {
         printf("[NODE] Alive! Id: %d, PID: %d \n", id, pid);
-        sleep(2);
+        sleep(10);
     }
 
     destroyNode(node);
