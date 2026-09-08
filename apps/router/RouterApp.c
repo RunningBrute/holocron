ac576b2ac2edf0627d0eae25a96919881fb58104
RunningBrute <piotrekbas8@gmail.com>
2026-09-08 23:25:23 +0200

    add initial server unix socket code to router

--- apps/router/RouterApp.c
+++ apps/router/RouterApp.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+
 #include "SocketPath.h"
 
 int main(int argc, const char* argv[])
@@ -16,6 +20,20 @@ int main(int argc, const char* argv[])
     socketPath.length = strlen(argv[1]);
 
     printf("[Router] socket adress: %s \n", socketPath.path);
+
+    int serverFd = socket(AF_UNIX, SOCK_STREAM, 0);
+
+    struct sockaddr_un addr = {0};
+    addr.sun_family = AF_UNIX;
+    strncpy(addr.sun_path, socketPath.path, sizeof(addr.sun_path) - 1);
+
+    unlink(socketPath.path);
+
+    bind(serverFd, (struct sockaddr*)&addr, sizeof(addr));
+    listen(serverFd, SOMAXCONN);
+
+    // TODO
+    //int node_fd = accept(server_fd, NULL, NULL);
 
     return 0;
 }
